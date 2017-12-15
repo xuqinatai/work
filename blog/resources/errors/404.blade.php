a564e3b27986e4e755b876bc4f9944b277b990a0
xuqinatai <xuqiantai@15w.com>
2017-12-15 14:26:33 +0800

    blog

---
+++ blog/resources/errors/404.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<center>
+    <img src="http://st2.jingniukeji.com/img/d6ffb24ad6c8d211.jpg" alt="" style="text-align: center;margin-top: auto">
+</center>
+
+</body>
+</html>
